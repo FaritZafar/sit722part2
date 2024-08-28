5c0700e1dc75a9369e7b2a9a4f32fa1b6072cbd7
Farit Ali Zafar <77563804+FaritZafar@users.noreply.github.com>
2024-08-28 22:08:41 +1000

    Update config.py

--- book_catalog/config.py
+++ book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://book_catalog_db_hg7v_user:xYRp6A9Ns6wVPtcz9B1nIykuoLod8Yc1@dpg-cr7grsa3esus7389kkf0-a:5432/book_catalog_db_hg7v")
 
 settings = Settings()
